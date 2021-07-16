2014301b28b4e610619061df6ef8e1a1b64346f0
anushkasahoo <85781997+anushkasahoo@users.noreply.github.com>
2021-07-16 16:23:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function skeletal() 
+{
+    window.location = "skeletal.html";
+}
+function respiratory() 
+{
+    window.location = "respiratory.html";
+}
+function nervous() 
+{
+    window.location = "nervous.html";
+}
+function muscular() 
+{
+    window.location = "muscular.html";
+}
+function excretory() 
+{
+    window.location = "excretory.html";
+}
